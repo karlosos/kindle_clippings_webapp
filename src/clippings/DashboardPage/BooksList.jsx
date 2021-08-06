5cc52a400e457f38143dd713b7183aee17cfcdca
Karol Dzialowski <karlososhd@gmail.com>
2021-08-06 20:55:37 +0200

    Dashboard pagination

--- src/clippings/DashboardPage/BooksList.jsx
+++ src/clippings/DashboardPage/BooksList.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BookItem from './BookItem'
 import styled from 'styled-components'
 import { useSelector, useDispatch } from 'react-redux'
+import { Pagination } from 'semantic-ui-react'
 
 const BooksContainer = styled.div`
     display: flex;
     flex-direction: column;
 `
 
+const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 const BooksList = () => {
   const books = useSelector((state) => state.clippings.books)
-  
+  const [activePage, setActivePage] = useState(1)
+
+  // pagination
+  const itemsPerPage = 20
+  const numPages = useSelector((state) => Math.ceil(books.length / itemsPerPage))
+  const booksFiltered = books.slice((activePage - 1) * itemsPerPage, activePage * itemsPerPage)
+  const handlePaginationChange = (e, { activePage }) => {
+    setActivePage(activePage)
+    window.scrollTo(0, 0)
+  }
+
   return (
-    <BooksContainer>
-      {books.map((book) => (
-        <BookItem key={book.id} book={book} />
-      ))}
-    </BooksContainer>
+    <>
+      <BooksContainer>
+        {booksFiltered.map((book) => (
+          <BookItem key={book.id} book={book} />
+        ))}
+      </BooksContainer>
+      <PaginationWrapper>
+        <Pagination
+          activePage={activePage}
+          onPageChange={handlePaginationChange}
+          totalPages={numPages}
+        />
+      </PaginationWrapper>
+    </>
   )
 }
 
