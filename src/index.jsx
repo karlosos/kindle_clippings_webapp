fe0a9a091411ca563cfe6f072af5827a8a546ee8
Karol Dzialowski <karlososhd@gmail.com>
2024-03-19 21:23:20 +0100

    Upgrade minor packages

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 /* istanbul ignore file */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './app/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
